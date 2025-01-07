268c5ce2f27ddc01b785359d00776d6c77ad1da5
Javier Calartrava <javiercalatrava@capitole-consulting.com>
2025-01-07 21:04:56 +0100

    Initial Commit

---
+++ AgnosticSwiftData/AgnosticSwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  AgnosticSwiftDataApp.swift
+//  AgnosticSwiftData
+//
+//  Created by Javier Calatrava on 7/1/25.
+//
+
+import SwiftUI
+
+@main
+struct AgnosticSwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AgnosticSwiftData/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AgnosticSwiftData
+//
+//  Created by Javier Calatrava on 7/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
